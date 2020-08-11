d2f001efe048e19d432781bc3ef983cb32688fd9
Mert Ceylan <alimrtcyln@gmail.com>
2020-08-11 23:42:24 +0300

    Dev
    - Adding tests for broadcast operations.

--- test/brdcstng.jl
+++ test/brdcstng.jl
@@ -2,6 +2,16 @@
 
 using Test
 
+@testset "broadcasting" begin
+    
+    a = BD{Array{Int}}(zeros(Int,3,3))
+
+    @testset "number_to_bdnumber" begin
+        y, dy = (1 .+ a).f
+        @test dy(1) == (1.0, [1 1 1; 1 1 1; 1 1 1])
+    end
+end
+
 # int bdarrayint
 # int bdarrayfloat
 # float bdarrayfloat
