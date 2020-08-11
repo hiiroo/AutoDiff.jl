650b847ad021adc89752f03ed35d292b777302bd
Mert Ceylan <alimrtcyln@gmail.com>
2020-08-11 23:42:02 +0300

    Cosmetic
    -Revisited promotion tests.

--- test/promtn.jl
+++ test/promtn.jl
@@ -2,20 +2,21 @@
 
 using Test
 
-@testset "number_promotions" begin
+@testset "promotion" begin
+    @testset "number_promotions" begin
     @test promote_type(Int, BD{Int}) == BD{Int64}
     @test promote_type(BD{Int}, Int) == BD{Int64}
 
     @test promote_type(Float64, BD{Float64}) == BD{Float64}
     @test promote_type(BD{Float64}, Float64) == BD{Float64}
-end
+    end
 
-@testset "wrapped_number_promotions" begin
+    @testset "wrapped_number_promotions" begin
     @test promote_type(BD{Int}, BD{Float64}) == BD{Float64}
     @test promote_type(BD{Float64}, BD{Int}) == BD{Float64}
-end
+    end
 
-@testset "wrappedarray_number_promotions" begin
+    @testset "wrappedarray_number_promotions" begin
     @test promote_type(BD{Array{Int}}, Int) == Any
     @test promote_type(BD{Int}, Array{Int}) == Any
 
@@ -27,4 +28,6 @@ end
 
     @test promote_type(BD{Array{Int}}, BD{Float64}) == Any
     @test promote_type(BD{Array{Float64}}, BD{Array{Int}}) == BD{Array{Float64}}
+    end
+
 end
